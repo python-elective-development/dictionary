cc51fe1a2ea3f4580db17b344d03528182358660
clbokea <clbo@kea.dk>
2018-12-07 15:00:35 +0100

    one page init

--- menu.py
+++ menu.py
@@ -1,33 +1,23 @@
 import func
-import menu_functions
 
-#menu = {1:'Home', 2:'About', 'Q': 'Quit'}
-menu = []
+menu = {1:'Home', 2:'About', 'Q': 'Quit'}
+
 def usr_input(name):
     npt = input("Hi " + name +", What do you want to do? \n(" + str(menu) + ")") 
     return npt.lower()
 
-# def home(name):
-#     string = 'Hi ' + name + ' This is my HOME page'
-#     return string
+def home(name):
+    string = 'Hi ' + name + ' This is my HOME page'
+    return string
 
-# def about(name):
-#     string = 'Hi ' + name + ' This is my ABOUT page'
-#     return string
+def about(name):
+    string = 'Hi ' + name + ' This is my ABOUT page'
+    return string
 
-# def quit(name):
-#     return "Goodbye " + name + " Sorry to see you go "
+def quit(name):
+    return "Goodbye " + name + " Sorry to see you go "
 
-dict_of_functions = {}
-# dict_of_functions = {'1':home, '2':about, 'q': quit }
-
-#count = 0
-for key, value in menu_functions.__dict__.items():
-    if callable(value):
-        dict_of_functions[key] = value
-        menu.append(key)  
-
-print(dict_of_functions)
+dict_of_functions = {'1':home, '2':about, 'q': quit }
 
 usr_name = input('Hi, Tell us your name! : ')
 
